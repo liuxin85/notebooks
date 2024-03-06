46c39313c5dbb9c4106f761c50e332401fe63d70
Xin Liu <liuxin85@gmail.com>
2024-03-06 09:59:47 +0800

    exercise 4

---
+++ stepbystep/v0.py
@@ -0,0 +1,188 @@
+
+import numpy as np
+import datetime
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+
+plt.style.use('fivethirtyeight')
+class StepByStep(object):
+    def __init__(self, model, loss_fn, optimizer):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        # Let's send the model to the specified device right away
+        self.model.to(self.device)
+        # These attributes are defined here, but since they are
+        # not avaible at the moment of creation, we keep them None
+        self.train_loader = None
+        self.val_loader = None
+        self.writer = None
+        # These attributes are going to be computed internally
+        self.losses = []
+        self.val_losses = []
+        self.total_epochs = 0
+        # Creates the train_step function for our model,
+        # loss function and optimizer
+        # Note: there are NO ARGS there! It makes use of the class attributes directly
+        self.train_step_fn = self._make_train_step_fn()
+        self.val_step_fn = self._make_val_step_fn()
+    
+
+    def to(self, device):
+        try:
+            self.device = device
+            self.model.to(self,device)
+        except RuntimeError:
+            self.device = ('cuda' if torch.cuda.is_available() else 'cpu')
+            print(f"Couldn't send it to {device}, sending it to {self.device} instead.")
+            self.model.to(self.device)
+
+    def set_loaders(self, train_loader, val_loader=None):
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+
+    def set_tensorboard(self, name, folder='runs'):
+        suffix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        self.writer = SummaryWriter(f'{folder}/{name}_{suffix}')
+    def _make_train_step_fn(self):
+        def perform_train_step_fn(x, y):
+            self.model.train()
+            yhat = self.model(x)
+            loss = self.loss_fn(yhat, y)
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            return loss.item()
+        return perform_train_step_fn
+
+    def _make_val_step_fn(self):
+        def perform_val_step_fn(x, y):
+            self.model.eval()
+            yhat = self.model(x)
+            loss = self.loss_fn(yhat, y)
+            return loss.item()
+
+        return perform_val_step_fn
+
+    def _mini_batch(self, validation=False):
+        # The mini-batch can be used with both loaders
+        # The argument `validation`defines which loader and 
+        # corresponding step function is going to be used
+        if validation:
+            data_loader = self.val_loader
+            step_fn = self.val_step_fn
+        else:
+            data_loader = self.train_loader
+            step_fn = self.train_step_fn
+
+        if data_loader is None:
+            return None
+            
+        # Once the data loader and step function, this is the same
+        # mini-batch loop we had before
+        mini_batch_losses = []
+        for x_batch, y_batch in data_loader:
+            x_batch = x_batch.to(self.device)
+            y_batch = y_batch.to(self.device)
+
+            mini_batch_loss = step_fn(x_batch, y_batch)
+            mini_batch_losses.append(mini_batch_loss)
+
+        loss = np.mean(mini_batch_losses)
+        return loss
+
+    def set_seed(self, seed=42):
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False    
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+    
+    def train(self, n_epochs, seed=42):
+        # To ensure reproducibility of the training process
+        self.set_seed(seed)
+
+        for epoch in range(n_epochs):
+            # Keeps track of the numbers of epochs
+            # by updating the corresponding attribute
+            self.total_epochs += 1
+
+            # inner loop
+            # Performs training using mini-batches
+            loss = self._mini_batch(validation=False)
+            self.losses.append(loss)
+
+            # VALIDATION
+            # no gradients in validation!
+            with torch.no_grad():
+                # Performs evaluation using mini-batches
+                val_loss = self._mini_batch(validation=True)
+                self.val_losses.append(val_loss)
+
+            # If a SummaryWriter has been set...
+            if self.writer:
+                scalars = {'training': loss}
+                if val_loss is not None:
+                    scalars.update({'validation': val_loss})
+                # Records both losses for each epoch under the main tag "loss"
+                self.writer.add_scalars(main_tag='loss',
+                                        tag_scalar_dict=scalars,
+                                        global_step=epoch)
+
+        if self.writer:
+            # Closes the writer
+            self.writer.close()
+    
+    def save_checkpoint(self, filename):
+        # Builds dictionary with all elements for resuming training
+        checkpoint = {'epoch': self.total_epochs,
+                      'model_state_dict': self.model.state_dict(),
+                      'optimizer_state_dict': self.optimizer.state_dict(),
+                      'loss': self.losses,
+                      'val_loss': self.val_losses}
+
+        torch.save(checkpoint, filename)
+
+    def load_checkpoint(self, filename):
+        # Loads dictionary
+        checkpoint = torch.load(filename)
+
+        # Restore state for model and optimizer
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        self.total_epochs = checkpoint['epoch']
+        self.losses = checkpoint['loss']
+        self.val_losses = checkpoint['val_loss']
+
+        self.model.train() # always use TRAIN for resuming training   
+
+    def predict(self, x):
+        # Set is to evaluation mode for predictions
+        self.model.eval() 
+        # Takes aNumpy input and make it a float tensor
+        x_tensor = torch.as_tensor(x).float()
+        # Send input to device and uses model for prediction
+        y_hat_tensor = self.model(x_tensor.to(self.device))
+        # Set it back to train mode
+        self.model.train()
+        # Detaches it, brings it to CPU and back to Numpy
+        return y_hat_tensor.detach().cpu().numpy()
+
+    def plot_losses(self):
+        fig = plt.figure(figsize=(10, 4))
+        plt.plot(self.losses, label='Training Loss', c='b')
+        plt.plot(self.val_losses, label='Validation Loss', c='r')
+        plt.yscale('log')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.tight_layout()
+        return fig
+
+    def add_graph(self):
+        # Fetches a single mini-batch so we can use add_graph
+        if self.train_loader and self.writer:
+            x_sample, y_sample = next(iter(self.train_loader))
+            self.writer.add_graph(self.model, x_sample.to(self.device))
